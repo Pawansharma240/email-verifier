b93b33fb60af44f533ec8c18a3908c8da8266bb3
Pawansharma240 <pawansharma_mca23_25@its.edu.in>
2026-03-14 17:08:03 +0530

    fix package.json

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const verifyEmail = require("./verifyEmail");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Email Verifier API Running");
+});
+
+app.get("/verify", async (req, res) => {
+  const email = req.query.email;
+
+  if (!email) {
+    return res.json({ error: "Email parameter required" });
+  }
+
+  const result = await verifyEmail(email);
+
+  res.json(result);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
